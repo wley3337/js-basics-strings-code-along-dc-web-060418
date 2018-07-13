9f6e39a08cdec9285fdd6679b5d832afb14accfb
Will Ley <wley3337@gmail.com>
2018-07-13 23:59:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ const welcomeMessage = 'Welcome to Flatbook, ' + `${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const currentUser = 'Edsger Dijkstra';
+
+const shortGreeting = 'Welcome,   `${currentUser}`'
