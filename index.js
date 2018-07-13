bd6df03e83be907ca9d97c70eec1115699a9a555
Will Ley <wley3337@gmail.com>
2018-07-13 23:53:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 let currentUser = 'Grace Hopper'; 
 
-const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
