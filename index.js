4f01acce8b23b758f91ce7f6cb734819f7b914ca
Will Ley <wley3337@gmail.com>
2018-07-13 23:58:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ let currentUser = 'Grace Hopper';
 const welcomeMessage = 'Welcome to Flatbook, ' + `${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const currentUser = 'Edsger Dijkstra';
